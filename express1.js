badcabfe7f70406e08d37686c8c7378bb20eec15
UmungGarg <handsome.anmol5>
2023-08-22 16:56:03 +0530

    ExpressBasic

---
+++ express1.js
@@ -0,0 +1,16 @@
+
+const express= require('express');
+
+const app = express();
+
+app.use((req,res,next) => {
+    console.log('in the middleware');
+    next();
+})
+
+app.use((req,res,next) => {
+    console.log('in the other middleware');
+    res.send('<h1>Hello from Expres</h1>');
+})
+
+app.listen(3000);
