244c5e13dce6158f4e38e661756ceaee3bbd14fe
UmungGarg <handsome.anmol5>
2023-08-23 16:42:58 +0530

    InputusingMW

--- express1.js
+++ express1.js
@@ -1,16 +1,23 @@
-
 const express= require('express');
-
+const bodyParser= require('body-parser');
 const app = express();
 
-app.use((req,res,next) => {
-    console.log('in the middleware');
-    next();
-})
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req,res,next) => {
+
+app.use('/add-product', (req, res, next) => {
+    console.log('in the other ad middleware');
+    res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form></body></html>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     console.log('in the other middleware');
     res.send('<h1>Hello from Expres</h1>');
-})
+});
 
 app.listen(3000);
