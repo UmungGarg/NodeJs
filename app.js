46163f8f9a5a59ed64744e6adaed263d5b36e4f0
UmungGarg <handsome.anmol5>
2023-10-21 00:32:16 +0530

    userProductRelationship

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./util/database'); 
 const errorController = require('./controllers/error');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -24,14 +26,37 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints:true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize.sync()
+// .sync({force:true})
 .then(result => {
-    console.log(result);
+    return User.findByPk(1);
+    // console.log(result);
+})
+.then(user => {
+    if(!user) {
+        return User.create({ name:'Max', email:'test@tst.com'});
+    }
+    return user;
+})
+.then(user => {
+    console.log(user);
     app.listen(3000);
 })
 .catch(err => {
