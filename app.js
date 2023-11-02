9c7e71aee909b333f6fdabd8dc991faa6b33f370
UmungGarg <handsome.anmol5>
2023-11-02 16:35:41 +0530

    manyManyRelationship

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const bodyParser = require('body-parser');
 const sequelize = require('./util/database'); 
 const errorController = require('./controllers/error');
 const Product = require('./models/product');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 const User = require('./models/user');
+
 
 const app = express();
 
@@ -42,8 +46,13 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints:true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem});
+Product.belongsToMany(Cart, { through: CartItem});
 
-sequelize.sync()
+sequelize
+.sync()
 // .sync({force:true})
 .then(result => {
     return User.findByPk(1);
@@ -56,7 +65,10 @@ sequelize.sync()
     return user;
 })
 .then(user => {
-    console.log(user);
+    // console.log(user);
+    return user.createCart();
+})
+.then(cart => {
     app.listen(3000);
 })
 .catch(err => {
